ab06d659cc3aaabf0d7d2e4b1f3bbc020b4f4266
kevinramirez723 <kevinramirez723@gmail.com>
2023-03-26 05:31:04 +0000

    active tab edge case test

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ function buildNav(selector) {
     $(selector).load("nav.html", function(){
         var url_lst = window.location.href.split("/");
         var active = url_lst[url_lst.length - 1];
+        console.log(active);
+        if (active == "kevinramirez723.github.io") {
+            active = "index.html";
+        }
         $(`a[href="${active}"]`).addClass('active').attr('aria-current', 'page');
     });
 }
