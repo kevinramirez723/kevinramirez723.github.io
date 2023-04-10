461bdbdd64076c2e53a4a7bf09d35f60bb2109e5
kevinramirez723 <kevinramirez723@gmail.com>
2023-04-10 03:58:20 +0000

    website complete overhaul started.

--- main.js
+++ main.js
@@ -8,3 +8,29 @@ function buildNav(selector) {
         $(`a[href="${active}"]`).addClass('active').attr('aria-current', 'page');
     });
 }
+
+
+$(document).ready(function() {
+    var clock;
+    $("#timerbutton").on('click', function() {
+        if ($(this).hasClass("active")) {
+            clearInterval(clock);
+            // TODO: make more efficient by only changing hour / minute when necessary
+            clock = setInterval(() => {
+                let date = new Date().toLocaleDateString("en-GB", {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    second: "2-digit"
+                }).slice(-8);
+                $(".hour").text(date.substring(0, 2));
+                $(".minute").text(date.substring(3, 5));
+                $(".second").text(date.substring(6));
+                $(".clock").css('visibility', 'visible');
+            }, 1000)
+        }
+        else {
+            $(".clock").css('visibility', 'hidden');
+            clearInterval(clock);
+        }
+    });
+});
